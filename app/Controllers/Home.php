c5855fe162d6da86fd2c4fba069bab6103d22bc1
hermantaniwan <taniwanherman@yahoo.com>
2024-03-31 11:13:18 +0700

    no message

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,18 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function loadDB()
+    {
+        $db = \Config\Database::connect();
+        $query   = $db->query('SELECT name, id FROM test');
+        $results = $query->getResult();
+
+        foreach ($results as $row) {
+            echo $row->name;
+            echo $row->id;
+        }
+
+        echo 'Total Results: ' . count($results);
+    }
 }
