7f10d0831353a6ae8da53f09f8b898e1a134edc7
hermantaniwan <taniwanherman@yahoo.com>
2024-04-14 09:36:52 +0700

    no message

--- app/Views/tiktok_template.php
+++ app/Views/tiktok_template.php
@@ -2,8 +2,7 @@
 
 <style>
     .wrap {
-        width: 550px;
-        height: 1000px;
+        width: 100%;
         padding: 0;
         overflow: hidden;
     }
@@ -13,11 +12,11 @@
         height: 786px;
         border: 0; */
 
-        -ms-transform: scale(1.25);
-        -moz-transform: scale(1.25);
-        -o-transform: scale(1.25);
-        -webkit-transform: scale(1.25);
-        transform: scale(1.25);
+        -ms-transform: scale(1.5);
+        -moz-transform: scale(1.5);
+        -o-transform: scale(1.5);
+        -webkit-transform: scale(1.5);
+        transform: scale(1.5);
 
         -ms-transform-origin: 0 0;
         -moz-transform-origin: 0 0;
@@ -29,7 +28,7 @@
 
 <body>
     <div class="wrap">
-        <iframe class='frame' src="https://www.tiktok.com/embed/v2/6718335390845095173" width="100%" height="199%" frameborder="0"></iframe>
+        <iframe class='frame' src="https://www.tiktok.com/embed/v2/6718335390845095173" width="100%" height="100%" frameborder="0"></iframe>
     </div>
 </body>
 
